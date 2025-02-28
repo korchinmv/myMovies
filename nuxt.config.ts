f36e042e939e8cb0ed5895c7d50a328470b0eee7
korchinmv <korchinmv@yandex.ru>
2025-02-28 17:12:22 +0300

    fix: fancybox #4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,7 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
 	css: ["~/assets/scss/main.scss"],
-	plugins: [
-		"~/plugins/useClickOutside.client.ts",
-		"~/plugins/useFancybox.client.ts",
-	],
+	plugins: ["~/plugins/useClickOutside.ts", "~/plugins/useFancybox.ts"],
 	modules: [
 		"@pinia/nuxt",
 		"@vueuse/nuxt",
